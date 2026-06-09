65e7a2442f7e75790e18958bf765c3fc1242aa3f
Gustavo Fortini <gftmoreira@sga.pucminas.br>
2026-06-09 08:39:56 -0300

    inicio encremento bd

--- public/assets/scripts/db.js
+++ public/assets/scripts/db.js
@@ -1,10 +1,8 @@
-// assets/scripts/db.js
+// public/assets/scripts/db.js
 const db = new Dexie('GaloFrameDB');
 
 db.version(1).stores({
-  memories: '++id, title, description, date, tags, imageData, createdAt',
-  users: 'id, name, username, avatar, bio',
-  // futuramente: comments, likes, etc.
+    memories: '++id, title, description, date, tags, imageData, createdAt'
 });
 
 export default db;
